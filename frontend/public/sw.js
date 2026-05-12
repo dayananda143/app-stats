363f22a7189975128b0791265a1a718f543b5376
daya <krdayananda1995@gmail.com>
2026-05-11 22:22:25 -0400

    Remove service worker completely

--- frontend/public/sw.js
+++
@@ -1,9 +0,0 @@
-// Self-destruct: unregister this SW and reload all clients so they get fresh files
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    self.registration.unregister()
-      .then(() => self.clients.matchAll({ type: 'window' }))
-      .then(clients => clients.forEach(client => client.navigate(client.url)))
-  );
-});
